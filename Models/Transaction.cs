75b7e4444a601e7210dd7911111bf7846c5b8f27
milan_w <milan_w@epiclanka.net>
2023-10-25 22:53:50 +0530

    Add my project

---
+++ Models/Transaction.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key] 
+        public int TransactionId { get; set; }
+
+        //CatogeryId
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
+        public string CategoryId { get; set; }
+        public Category Catogery { get; set; }
+
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
+        public int Amount { get; set; }
+
+
+
+        [Column(TypeName = "nvarchar(75")]
+        public String? Note { get; set; }
+
+
+
+        public DateTime Date { get; set;} = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Catogery == null ? "" : Catogery.Icon + " " + Catogery.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Catogery == null || Catogery.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
+
+    }
+}
